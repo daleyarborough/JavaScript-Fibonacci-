3419be6ef04d82b183266f0ed4aa1ec041faf2db
Dale Yarborough <daleyarborough@gmail.com>
2016-05-06 16:59:16 -0400

    Create index.js

---
+++ index.js
@@ -0,0 +1,16 @@
+# JavaScript-Fibonacci-
+var yourself = {
+    fibonacci : function(n) {
+        if (n === 0 || n === 1)
+        return n;
+    {
+            return fibonacci(n - 1) + fibonacci(n - 2);
+        }
+        
+        if (n === 1) {
+            return 1;
+        }
+    }
+};
+
+// Still need to define fibonacci //
