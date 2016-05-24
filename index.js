c1d459426f5141118bfef6ccca2268e8c8aa09bb
Dale Yarborough <daleyarborough@gmail.com>
2016-05-24 16:42:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 # JavaScript-Fibonacci-
-var yourself = {
-    fibonacci : function(n) {
-        if (n === 0 || n === 1)
-        return n;
-    {
-            return fibonacci(n - 1) + fibonacci(n - 2);
-        }
-        
-        if (n === 1) {
-            return 1;
-        }
+var recursive = function(n) {
+    if(n <= 2) {
+        return 1;
+    } else {
+        return this.recursive(n - 1) + this.recursive(n - 2);
     }
 };
 
-// Still need to define fibonacci //
+
